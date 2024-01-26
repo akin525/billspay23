99dbf30f13af66f19d5caf769049d1b9ddf5f6b6
PIPER <akinlabi@gmail.com>
2024-01-26 19:20:25 +0100

    base

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -208,40 +208,6 @@
 {{--            </a>--}}
 {{--        </div>--}}
 
-
-        <div class="col-xl-3  col-lg-6 col-sm-6">
-            <div class="widget-stat card">
-                <div class="card-body p-4">
-                    <div class="media ai-icon">
-									<span class="me-3 bgl-primary text-primary">
-										<!-- <i class="ti-user"></i> -->
-										<i class="fa fa-money"></i>
-									</span>
-                        <div class="media-body">
-                            <p class="mb-1">Wallet</p>
-                            <h4 class="mb-0">₦{{number_format(intval($wallet['balance'] *1),2)}}</h4>
-                            <span class="badge badge-primary">Balance</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-3  col-lg-6 col-sm-6">
-            <div class="widget-stat card">
-                <div class="card-body p-4">
-                    <div class="media ai-icon">
-									<span class="me-3 bgl-warning text-warning">
-                                        <i class="fa fa-money"></i>
-									</span>
-                        <div class="media-body">
-                            <p class="mb-1">Deposit</p>
-                            <h4 class="mb-0">₦{{number_format(intval($tdepo *1),2)}}</h4>
-                            <span class="badge badge-warning">Total</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
         <div class="col-xl-3  col-lg-6 col-sm-6">
             <div class="widget-stat card">
                 <div class="card-body  p-4">
